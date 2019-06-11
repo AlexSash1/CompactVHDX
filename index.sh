476cb82f96e44d9001fce504823c86a315c24894
root <root@debian-testserver>
2019-06-12 02:46:40 +1000

    corrrect file size

--- index.sh
+++ index.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
-dd if=/dev/zero of=/tmp/null.file ; rm -f /tmp/null.file
+#dd if=/dev/zero of=/tmp/null.file ; rm -f /tmp/null.file
+
+
+
+# dd if=/dev/fd0 of=floppy.img bs=1474560
+#df| grep sd | awk ' {print $4} '
+bkb=`df| grep sd | awk '{print $4}'`
+sizenullfilekb=`echo $bkb-10000000 | /usr/bin/bc -l`
+
+sizenullfileb=`echo $sizenullfilekb | /usr/bin/bc -l`
+
+echo $sizenullfileb
+
+dd if=/dev/zero of=/tmp/null.file bs=1024K seek=$sizenullfileb
+rm -f /tmp/null.file
+
