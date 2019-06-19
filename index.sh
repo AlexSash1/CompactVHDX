adec6649866609a0febcf36ad26293f830c89dfd
root <root@debian-backup.vvo.local>
2019-06-19 15:43:05 +1000

    testing version

--- index.sh
+++ index.sh
@@ -8,10 +8,15 @@
 bkb=`df| grep sd | awk '{print $4}'`
 sizenullfilekb=`echo $bkb-10000000 | /usr/bin/bc -l`
 
-sizenullfileb=`echo $sizenullfilekb | /usr/bin/bc -l`
+sizenullfileMb=`echo "$sizenullfilekb/1024" | /usr/bin/bc -l`
 
-echo $sizenullfileb
+#echo $sizenullfileMb
 
-dd if=/dev/zero of=/tmp/null.file bs=1024K seek=$sizenullfileb
+sizenullfileMb_cuted=`echo $sizenullfileMb | cut -f 1 -d .`
+
+echo $sizenullfileMb_cuted
+
+#dd if=/dev/zero of=/tmp/null.file bs=1024K seek=$sizenullfileMb_cuted
+dd if=/dev/zero of=/tmp/null.file bs=1 count=0 seek=`echo $sizenullfileMb_cuted`M
 rm -f /tmp/null.file
 
