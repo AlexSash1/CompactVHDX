82aeb86c602d729cee1833771f00549f0d37bdf4
root <root@debian-backup.vvo.local>
2019-06-19 16:28:39 +1000

    add comments

--- index.sh
+++ index.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
-#dd if=/dev/zero of=/tmp/null.file ; rm -f /tmp/null.file
 
-
-
-# dd if=/dev/fd0 of=floppy.img bs=1474560
-#df| grep sd | awk ' {print $4} '
+#check free size on partitian "sd" there means that we have only one=)
 bkb=`df| grep sd | awk '{print $4}'`
+
+#set size of file for writing in Kb 
 sizenullfilekb=`echo $bkb-10000000 | /usr/bin/bc -l`
 
-sizenullfileMb=`echo "$sizenullfilekb/1024" | /usr/bin/bc -l`
+#this size in Gb
+sizenullfileGb=`echo "$sizenullfilekb/1048576" | /usr/bin/bc -l`
 
-#echo $sizenullfileMb
+#cut all after dot and dot too
+sizenullfileGb_cuted=`echo $sizenullfileGb | cut -f 1 -d .`
 
-sizenullfileMb_cuted=`echo $sizenullfileMb | cut -f 1 -d .`
+#log this size
+echo $sizenullfileGb size of writing file in Gygabyte
 
-echo $sizenullfileMb_cuted
+#write file from zeroes
+dd if=/dev/zero of=/tmp/null.file bs=1G count=$sizenullfileGb_cuted
 
-#dd if=/dev/zero of=/tmp/null.file bs=1024K seek=$sizenullfileMb_cuted
-dd if=/dev/zero of=/tmp/null.file bs=1 count=0 seek=`echo $sizenullfileMb_cuted`M
+#delete file
 rm -f /tmp/null.file
 
